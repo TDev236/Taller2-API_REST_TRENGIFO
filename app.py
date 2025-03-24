61e1f12ec21ff7604a4cdf563cd348dd45c232ff
Tiffani A. Rengifo Mori <tiffanirengifo23@gmail.com>
2025-03-23 23:29:26 -0500

    updated again

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
@@ -49,5 +50,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
